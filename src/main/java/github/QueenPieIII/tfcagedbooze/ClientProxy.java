532703c53caeea95e85180ff14e6b2bcd10e0b75
QueenPieIII <hsnhnhalimeh@gmail.com>
2023-02-09 23:53:43 +0200

    removed cirno

--- src/main/java/github/QueenPieIII/tfcagedbooze/ClientProxy.java
+++ src/main/java/github/QueenPieIII/tfcagedbooze/ClientProxy.java
@@ -1,15 +1,15 @@
 package github.QueenPieIII.tfcagedbooze;
 
-import cpw.mods.fml.client.registry.ClientRegistry;
+//import cpw.mods.fml.client.registry.ClientRegistry;
 
-import github.QueenPieIII.tfcagedbooze.render.*;
-import github.QueenPieIII.tfcagedbooze.tile.*;
+//import github.QueenPieIII.tfcagedbooze.render.*;
+//import github.QueenPieIII.tfcagedbooze.tile.*;
 
 public class ClientProxy extends CommonProxy
 {
     @Override
     public void registerTileEntities()
     {
-        ClientRegistry.registerTileEntity(TileCirno.class, "Cirno", new RenderCirno());
+        //ClientRegistry.registerTileEntity(TileCirno.class, "Cirno", new RenderCirno());
     }
 }
